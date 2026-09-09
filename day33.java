039699afc23d5b8a180c652cea8c6a7b6aed8865
Magasri-Sivakumar <magasrisivakumar07@gmail.com>
2026-09-09 06:19:42 +0530

    Add solutions for problems 78 and 448
    Implemented solutions for problems 78 and 448, including subsets generation and finding disappeared numbers in an array.

---
+++ day33.java
@@ -0,0 +1,39 @@
+Problem No:78
+Problem:Subsets
+Solution:
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+          List<List<Integer>> result = new ArrayList<>();
+        backtrack(0, nums, new ArrayList<>(), result);
+        return result;
+    }
+    private void backtrack(int index, int[] nums, List<Integer> current, List<List<Integer>> result) {
+        result.add(new ArrayList<>(current));
+        for (int i = index; i < nums.length; i++) {
+            current.add(nums[i]);
+            backtrack(i + 1, nums, current, result);
+            current.remove(current.size() - 1);
+        }
+    }
+}
+
+Problem No:448
+Problem:Find All Numbers Disappeared in an Array
+Solution:
+class Solution {
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] > 0) {
+                nums[index] = -nums[index];
+            }
+        }
+        List<Integer> result = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                result.add(i + 1);
+            }
+        }
+        return result;
+    }
+}
